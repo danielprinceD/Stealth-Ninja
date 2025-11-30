1bf2f96d6d21e7a9703881bcbdaae15a2ff5e59c
danielprinced <d.danielprince2003@gmail.com>
2025-11-30 20:47:12 +0530

    Watcher Movement Added

--- Assets/Controllers/WatcherController.cs
+++ Assets/Controllers/WatcherController.cs
@@ -1,14 +1,39 @@
 using System.Collections;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class WatcherController : MonoBehaviour
 {
+    public int speed = 5;
+    public float delayTime = 1;
     public Transform pathObject;
     void Start()
     {
-        
+        Vector3[] waypoints = new Vector3[pathObject.childCount];
+        for(int i = 0; i < pathObject.childCount; i++) {
+            waypoints[i] = pathObject.GetChild(i).position;
+        }
+        StartCoroutine(ReachPoints(waypoints));
     }
+
+    IEnumerator ReachPoints(Vector3[] reachPoints)
+    {
+        int targetIndex = 1;
+        while (true)
+        {
+            Vector3 reachPoint = reachPoints[targetIndex] + Vector3.up * transform.localScale.y;
+            float angle = 90-Mathf.Atan2(reachPoint.z , reachPoint.x) * Mathf.Rad2Deg;
+            transform.position = Vector3.MoveTowards(transform.position , reachPoint  , speed * Time.deltaTime);
+            transform.eulerAngles = transform.TransformDirection(Vector3.up * angle);
+            if(transform.position == reachPoint)
+            {
+                targetIndex += 1;
+                targetIndex = targetIndex % reachPoints.Length;
+                yield return new WaitForSeconds(delayTime);
+            }
+            yield return null;
+        }
+    }
+    
 
     void OnDrawGizmos()
     {
