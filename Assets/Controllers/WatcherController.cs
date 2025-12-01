8369375e079a7aa9d1a25c8fb65599ec36816233
danielprinced <d.danielprince2003@gmail.com>
2025-12-01 23:43:18 +0530

    Started Watcher Ability

--- Assets/Controllers/WatcherController.cs
+++ Assets/Controllers/WatcherController.cs
@@ -5,6 +5,7 @@ public class WatcherController : MonoBehaviour
 {
     public int speed = 5;
     public float delayTime = 1;
+    public float turnDelay = 0.5f;
     public Transform pathObject;
     void Start()
     {
@@ -15,19 +16,36 @@ public class WatcherController : MonoBehaviour
         StartCoroutine(ReachPoints(waypoints));
     }
 
+    IEnumerator TurnAngle(float angle)
+    {
+        Vector3 angleVector = Vector3.up * angle;
+        while (true)
+        {
+            Vector3.Angle(transform.eulerAngles , angleVector);
+            print(transform.eulerAngles);
+            if(transform.eulerAngles == angleVector)
+            {
+                yield return null;
+            }
+        }
+    }
+
     IEnumerator ReachPoints(Vector3[] reachPoints)
     {
         int targetIndex = 1;
+        Vector3 reachPoint = reachPoints[targetIndex] + Vector3.up * transform.localScale.y;
         while (true)
         {
-            Vector3 reachPoint = reachPoints[targetIndex] + Vector3.up * transform.localScale.y;
-            float angle = 90-Mathf.Atan2(reachPoint.z , reachPoint.x) * Mathf.Rad2Deg;
             transform.position = Vector3.MoveTowards(transform.position , reachPoint  , speed * Time.deltaTime);
-            transform.eulerAngles = transform.TransformDirection(Vector3.up * angle);
+            
             if(transform.position == reachPoint)
             {
                 targetIndex += 1;
                 targetIndex = targetIndex % reachPoints.Length;
+                reachPoint = reachPoints[targetIndex] + Vector3.up * transform.localScale.y;
+                Vector3 distanceVector = reachPoint - gameObject.transform.position;
+                float angle = 90-Mathf.Atan2(distanceVector.z , distanceVector.x) * Mathf.Rad2Deg;
+                yield return StartCoroutine(TurnAngle(angle));
                 yield return new WaitForSeconds(delayTime);
             }
             yield return null;
