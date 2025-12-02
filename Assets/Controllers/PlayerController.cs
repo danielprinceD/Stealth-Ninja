feb4fe1343a7c9971bb57c48d4502c975fc2db52
danielprinced <d.danielprince2003@gmail.com>
2025-12-02 23:41:34 +0530

    Lerp Angle Added for Player

--- Assets/Controllers/PlayerController.cs
+++ Assets/Controllers/PlayerController.cs
@@ -5,9 +5,13 @@ public class PlayerController : MonoBehaviour
 {
     public int speed = 7;
     public float smoothTime = .1f;
+    public float turnSpeed = 5;
 
     float smoothInputMagnitude;
     float smoothVelocity;
+
+    float CurrentAngle;
+
     void Update()
     {
         float HorizontalControl = Input.GetAxisRaw("Horizontal");
@@ -17,6 +21,7 @@ public class PlayerController : MonoBehaviour
         smoothInputMagnitude = Mathf.SmoothDamp( smoothInputMagnitude , directionVector.magnitude , ref smoothVelocity , smoothTime );
         
         float angle = Mathf.Atan2( directionVector.x , directionVector.z ) * Mathf.Rad2Deg;
+        CurrentAngle = Mathf.LerpAngle( CurrentAngle , angle , Time.deltaTime * turnSpeed );
         transform.eulerAngles = Vector3.up * angle;
         
         transform.Translate( transform.forward * speed * Time.deltaTime * smoothInputMagnitude , Space.World );
