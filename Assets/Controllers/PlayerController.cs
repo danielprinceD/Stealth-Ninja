03789db6065c2aad6436ef9ddbfc9614a201b1af
danielprinced <d.danielprince2003@gmail.com>
2025-12-02 23:20:15 +0530

    Player Controller Added With Rotation of Player with Respect to World

--- Assets/Controllers/PlayerController.cs
+++ Assets/Controllers/PlayerController.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int speed = 7;
     void Update()
     {
-        
+        float HorizontalControl = Input.GetAxisRaw("Horizontal");
+        float VerticalControl = Input.GetAxisRaw("Vertical");
+        Vector3 directionVector = new Vector3( HorizontalControl , 0 , VerticalControl).normalized;
+        float angle = Mathf.Atan2( directionVector.x , directionVector.z ) * Mathf.Rad2Deg;
+        transform.eulerAngles = Vector3.up * angle;
+        transform.Translate( transform.forward * speed * Time.deltaTime * directionVector.magnitude , Space.World );
     }
 }
