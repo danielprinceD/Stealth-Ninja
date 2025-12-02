c49962f00c1513940b110110b3c1c500869b758d
danielprinced <d.danielprince2003@gmail.com>
2025-12-02 21:41:46 +0530

    Turn Angle For Watcher Completed

--- Assets/Controllers/WatcherController.cs
+++ Assets/Controllers/WatcherController.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class WatcherController : MonoBehaviour
 {
     public int speed = 5;
-    public float delayTime = 1;
+    public int WatcherRotationSpeed = 10;
     public float turnDelay = 0.5f;
     public Transform pathObject;
     void Start()
@@ -15,38 +16,33 @@ public class WatcherController : MonoBehaviour
         }
         StartCoroutine(ReachPoints(waypoints));
     }
-
     IEnumerator TurnAngle(float angle)
     {
-        Vector3 angleVector = Vector3.up * angle;
-        while (true)
+        
+        while (Mathf.DeltaAngle(transform.eulerAngles.y, angle) != 0)
         {
-            Vector3.Angle(transform.eulerAngles , angleVector);
-            print(transform.eulerAngles);
-            if(transform.eulerAngles == angleVector)
-            {
-                yield return null;
-            }
+            float changeInAngle = Mathf.MoveTowardsAngle( transform.eulerAngles.y , angle , WatcherRotationSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * changeInAngle;
+            yield return null;
         }
+        yield return null;
     }
-
     IEnumerator ReachPoints(Vector3[] reachPoints)
     {
         int targetIndex = 1;
-        Vector3 reachPoint = reachPoints[targetIndex] + Vector3.up * transform.localScale.y;
+        transform.LookAt(reachPoints[targetIndex] + Vector3.up * transform.localScale.y);
         while (true)
         {
+            Vector3 reachPoint = reachPoints[targetIndex] + Vector3.up * transform.localScale.y;
             transform.position = Vector3.MoveTowards(transform.position , reachPoint  , speed * Time.deltaTime);
-            
             if(transform.position == reachPoint)
             {
                 targetIndex += 1;
                 targetIndex = targetIndex % reachPoints.Length;
-                reachPoint = reachPoints[targetIndex] + Vector3.up * transform.localScale.y;
-                Vector3 distanceVector = reachPoint - gameObject.transform.position;
+                reachPoint = reachPoints[targetIndex];
+                Vector3 distanceVector = (reachPoint - transform.position).normalized;
                 float angle = 90-Mathf.Atan2(distanceVector.z , distanceVector.x) * Mathf.Rad2Deg;
                 yield return StartCoroutine(TurnAngle(angle));
-                yield return new WaitForSeconds(delayTime);
             }
             yield return null;
         }
@@ -60,7 +56,7 @@ public class WatcherController : MonoBehaviour
 
         foreach(Transform waypoint in pathObject)
         {
-            Gizmos.color = Color.black;
+            Gizmos.color = Color.darkBlue;
             Gizmos.DrawSphere(waypoint.position , .1f);
             Gizmos.color = Color.green;
             Gizmos.DrawLine(prevPosition , waypoint.position);
