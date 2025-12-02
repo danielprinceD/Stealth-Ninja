586a16bdbf541f94df71ddbbe6f2579c6e0e15d4
danielprinced <d.danielprince2003@gmail.com>
2025-12-02 23:35:39 +0530

    SmoothDamp Added For Moving Direction

--- Assets/Controllers/PlayerController.cs
+++ Assets/Controllers/PlayerController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int speed = 7;
+    public float smoothTime = .1f;
+
+    float smoothInputMagnitude;
+    float smoothVelocity;
     void Update()
     {
         float HorizontalControl = Input.GetAxisRaw("Horizontal");
         float VerticalControl = Input.GetAxisRaw("Vertical");
+        
         Vector3 directionVector = new Vector3( HorizontalControl , 0 , VerticalControl).normalized;
+        smoothInputMagnitude = Mathf.SmoothDamp( smoothInputMagnitude , directionVector.magnitude , ref smoothVelocity , smoothTime );
+        
         float angle = Mathf.Atan2( directionVector.x , directionVector.z ) * Mathf.Rad2Deg;
         transform.eulerAngles = Vector3.up * angle;
-        transform.Translate( transform.forward * speed * Time.deltaTime * directionVector.magnitude , Space.World );
+        
+        transform.Translate( transform.forward * speed * Time.deltaTime * smoothInputMagnitude , Space.World );
     }
 }
